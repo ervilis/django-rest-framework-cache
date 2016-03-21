9f2f13ef20f89311cd28b51e2e49987c9e25383f
Ervilis Souza <ervilisviana@gmail.com>
2016-03-21 16:58:45 -0300

    Adding settings module

---
+++ rest_framework_cache/settings.py
@@ -0,0 +1,54 @@
+"""
+Settings for REST framework cache are all namespaced in the
+REST_FRAMEWORK_CACHE setting. For example your project's `settings.py` file
+might look like this:
+
+REST_FRAMEWORK_CACHE = {
+    'DEFAULT_CACHE_TIMEOUT': 86400
+}
+
+This module provides the `api_setting` object, that is used to access
+REST framework cache settings, checking for user settings first, then falling
+back to the defaults.
+"""
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.test.signals import setting_changed
+
+
+DEFAULTS = {
+    'DEFAULT_CACHE_TIMEOUT': 86400,
+}
+
+
+class APISettings(object):
+
+    """
+    A settings object, that allows API settings to be accessed as properties.
+    For example:
+
+        from rest_framework_cache.settings import api_settings
+        print(api_settings.DEFAULT_CACHE_TIMEOUT)
+
+    """
+
+    def __init__(self, defaults=None):
+        self.defaults = defaults
+        self.settings = getattr(settings, 'REST_FRAMEWORK_CACHE', {})
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults:
+            raise AttributeError("Invalid API setting: '%s'" % attr)
+
+        try:
+            # Check if present in user settings
+            val = self.settings[attr]
+        except KeyError:
+            # Fall back to defaults
+            val = self.defaults[attr]
+
+        return val
+
+
+api_settings = APISettings(DEFAULTS)
