25f7eee95bc132e7ee834b7a837f6648b7444fd0
Ervilis Souza <ervilisviana@gmail.com>
2016-03-21 23:19:42 -0300

    Registering serializer when CachedSerializerMixin is declared

--- rest_framework_cache/serializers.py
+++ rest_framework_cache/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 
+from .cache import cache
+from .exceptions import AlreadyRegistered
+from .registry import registry
 from .settings import api_settings
 from .utils import get_cache_key
-from .cache import cache
 
 
 class CachedSerializerMixin(serializers.ModelSerializer):
@@ -12,7 +14,7 @@ class CachedSerializerMixin(serializers.ModelSerializer):
         Checks if the representation of instance is cached and adds to cache
         if is not.
         """
-        key = get_cache_key(instance, self)
+        key = get_cache_key(instance, self.__class__)
         cached = cache.get(key)
         if cached:
             return cached
@@ -20,3 +22,12 @@ class CachedSerializerMixin(serializers.ModelSerializer):
         result = super(CachedSerializerMixin, self).to_representation(instance)
         cache.set(key, result, api_settings.DEFAULT_CACHE_TIMEOUT)
         return result
+
+    def __new__(cls, *args, **kwargs):
+        """We must register all cached serializers into registry"""
+        klass = super(CachedSerializerMixin, cls).__new__(cls, *args, **kwargs)
+        try:
+            registry.register(cls)
+        except AlreadyRegistered:
+            pass
+        return klass
