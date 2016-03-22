1732b1932d8685bfa4f46992217e75673f6d5121
Ervilis Souza <ervilisviana@gmail.com>
2016-03-21 23:20:30 -0300

    Adding function to get all instance cache keys

--- rest_framework_cache/utils.py
+++ rest_framework_cache/utils.py
@@ -1,3 +1,4 @@
+from .registry import registry
 from .settings import api_settings
 
 
@@ -6,6 +7,15 @@ def get_cache_key(instance, serializer):
     params = {"id": instance.pk,
               "app_label": instance._meta.app_label,
               "model_name": instance._meta.object_name,
-              "serializer_name": serializer.__class__.__name__}
+              "serializer_name": serializer.__name__}
 
     return api_settings.SERIALIZER_CACHE_KEY_FORMAT.format(**params)
+
+
+def get_all_cache_keys(instance):
+    """Get all possibles cache keys for given instance"""
+    keys = []
+    serializers = registry.get(instance.__class__)
+    for serializer in serializers:
+        keys.append(get_cache_key(instance, serializer))
+    return keys
