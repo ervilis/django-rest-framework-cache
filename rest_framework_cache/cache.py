dc9ef3a3d2bbaec81bfb530508be15547380b084
Ervilis Souza <ervilisviana@gmail.com>
2016-03-21 23:17:52 -0300

    Changing cache module to use django caches only if the backend is not the default

--- rest_framework_cache/cache.py
+++ rest_framework_cache/cache.py
@@ -1,11 +1,13 @@
-from django.core.cache import caches
+from django.core.cache import cache, caches
 
 from .settings import api_settings
 from .exceptions import ImproperlyConfigured
 
 
 try:
-    cache = caches[api_settings.DEFAULT_CACHE_BACKEND]
+    cache_backend = api_settings.DEFAULT_CACHE_BACKEND
+    if cache_backend != "default":
+        cache = caches[cache_backend]
 except KeyError:
     raise ImproperlyConfigured("'{}' is a invalid CACHE_BACKEND".format(
         api_settings.DEFAUL_CACHE_BACKEND))
