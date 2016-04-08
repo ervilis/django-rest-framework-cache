be2c15b490ee4b3dbf42ba648fe08d2786e90d02
Ervilis Souza <ervilisviana@gmail.com>
2016-04-08 18:24:21 -0300

    Adding test for get_all_cache_keys

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,6 +1,8 @@
 import unittest
 
-from rest_framework_cache.utils import get_cache_key
+from rest_framework_cache.utils import get_cache_key, get_all_cache_keys
+from rest_framework_cache.registry import cache_registry
+
 from .models import TestModel
 from .serializers import TestSerializer
 
@@ -13,3 +15,16 @@ class GetCacheKeyTestCase(unittest.TestCase):
         serializer = TestSerializer()
         key = get_cache_key(instance, serializer.__class__, 'http')
         self.assertEqual(key, "http.tests.TestModel.TestSerializer:1000")
+
+
+class GetAllCacheKeyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        cache_registry.register(TestSerializer)
+
+    def test_ok(self):
+        instance = TestModel()
+        instance.id = 1000
+        keys = get_all_cache_keys(instance)
+        self.assertEqual(keys, ["http.tests.TestModel.TestSerializer:1000",
+                                "https.tests.TestModel.TestSerializer:1000"])
